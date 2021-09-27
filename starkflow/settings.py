362650676dcecebdf65b99a3c9b100dd76a63dd5
rajesh <rajeshthangaraj1@gmail.com>
2021-09-28 02:10:30 +0400

    celery task update

--- starkflow/settings.py
+++ starkflow/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
+from distutils.util import strtobool
 #from celery.schedules import crontab
 #import starkflow.task.tasks
 
@@ -24,12 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z6&lk)c08@rngk71-k)xdjl*4xs-yly1+-@%iw9_64g6r1n%gp'
+SECRET_KEY = os.environ.get("SECRET_KEY","django-insecure-z6&lk)c08@rngk71-k)xdjl*4xs-yly1+-@%iw9_64g6r1n%gp")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(strtobool(os.environ.get('DEBUG', 'True')))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS','*').split(' ')
+
 
 
 # Application definition
@@ -41,12 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'django.contrib.sites',
     'task',
     'celery',
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
-SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -60,12 +62,44 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5
+}
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=20),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': os.environ.get("SECRET_KEY","django-insecure-z6&lk)c08@rngk71-k)xdjl*4xs-yly1+-@%iw9_64g6r1n%gp"),
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
 ROOT_URLCONF = 'starkflow.urls'
--- task/views.py
+++ task/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from task.models import *
+from django.contrib.auth.models import User
 from .serializers import ConversionSerializer
 import requests
 from rest_framework import generics
@@ -11,46 +12,22 @@ from rest_framework.permissions import AllowAny, IsAdminUser
 
 class ConversionList(generics.ListCreateAPIView):
     queryset = Conversion.objects.all()
+    permission_classes = (IsAdminUser,)
 
     def get(self, request, format=None):
         conversion = Conversion.objects.all()
         serializer_class = ConversionSerializer(conversion, many=True)
         return Response(serializer_class.data)
-
-    permission_classes = (AllowAny,)
-    
+   
     def post(self, request):
         url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=AN34Z3ELH88WKNYA'
         r = requests.get(url)
         data = r.json()
         return Response(data)
 
-def index(request):   
+def index(request): 
 
-    url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=AN34Z3ELH88WKNYA'
-    r = requests.get(url)
-    data = r.json()
-    if data:
-        if data['Realtime Currency Exchange Rate']:
-            exchageobjects=data['Realtime Currency Exchange Rate']
-            from_currency_code=exchageobjects['1. From_Currency Code']
-            from_currency_name=exchageobjects['2. From_Currency Name']
-            to_currency_code=exchageobjects['3. To_Currency Code']
-            to_currency_name=exchageobjects['4. To_Currency Name']
-            exchange_rate=exchageobjects['5. Exchange Rate']
-            last_updated_rate=exchageobjects['6. Last Refreshed']
-
-            ## convertor rate create
-            conversiondb=Conversion()
-            conversiondb.from_currency_code=from_currency_code
-            conversiondb.to_currency_code=to_currency_code
-            conversiondb.from_currency_name=from_currency_name
-            conversiondb.to_currency_name=to_currency_name
-            conversiondb.exchange_rate=exchange_rate
-            #conversiondb.updated_at=last_updated_rate
-            conversiondb.save()
-    
-    #print(data[0]['From_Currency Name'])
-    return HttpResponse("Success")
+    #User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+    return HttpResponse("Using docker compose to create a celery periodic task")
 
 
